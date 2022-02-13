bd5a5e310884a7c0ad3b5b832841588c13fbe318
Daniil <68930860+DaniilBykau@users.noreply.github.com>
2022-02-13 22:03:29 +0100

    test 8

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QustionService} from "../service/qustion.service";
 
 @Component({
@@ -8,19 +8,52 @@ import {QustionService} from "../service/qustion.service";
 })
 export class QuestionsComponent implements OnInit {
 
-  public name : string="";
-  constructor(private qustionService : QustionService) { }
+  public name: string = "";
+  public randNum: number = 0;
+  public questionText: string = "";
+  public answerText: string = "";
+  public tooltipText: string = "";
+  public maxNumberOfQuestion: number = 4;
+  public questionsNumbers: any = [] ;
+  public questionExist:boolean = false;
+  constructor(private qustionService: QustionService) {
+  }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
+    this.findQuestion();
   }
 
-  getAllQuestions(){
+  getAllQuestions() {
+    for (let i = 0; i < this.maxNumberOfQuestion+1; i++) {
+      this.questionsNumbers?.push(i);
+    }
+  }
+
+  setQuestionData(){
     this.qustionService.getQuesionJson()
-      .subscribe(res=>{
-        console.log(res.questions);
+      .subscribe(res => {
+        this.questionText = res.questions[this.randNum].questionText
+        this.answerText = res.questions[this.randNum].answerText
       })
   }
 
+  findQuestion() {
+    this.questionExist=false;
+    this.randNum = Math.floor(Math.random() * (this.maxNumberOfQuestion + 1));
+    if(this.questionsNumbers.length==0){
+      this.tooltipText="cjnvjsnvjvndsjvn";
+    }
+    while(!this.questionExist) {
+      if (this.questionsNumbers?.includes(this.randNum)) {
+        this.questionExist = true;
+        this.setQuestionData()
+        delete this.questionsNumbers[this.randNum];
+      } else {
+        this.questionExist = false;
+        this.randNum = Math.floor(Math.random() * (this.maxNumberOfQuestion + 1));
+      }
+    }
+  }
 }
